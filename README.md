eb796bf0b504179814123db9d16ab945297dbffe
Mark Jeromin <mark.jeromin@sysfrog.net>
2018-07-03 17:08:17 -0400

    Added pull_test.sh

--- README.md
+++ README.md
@@ -12,6 +12,13 @@ First, generate some bloat:
 This will generate 124 bloat files each 10MB in size. The docker build will copy each file as an individual layer. I was able to build 127 layers with the *scratch* base layer and bloat layers:
 
 ```
-docker build -t maxlayers .
+docker image build -t maxlayers .
 ```
 
+## TEST
+The `pull_test.sh` script measures docker pull performance (counts for now). To run:
+```
+./pull_test.sh
+```
+
+Results will be printed on the console as well in *results.log*. The script runs forever, you will need to CNTL-C to cancel when you are satisfied with collecting enough data.
---
+++ pull_test.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# Description: This script measures docker pull performance
+# WARNING: This will prune ALL of your image cache.
+
+log="results.log"
+image="mjeromin/maxlayers"
+
+ATTEMPT=0
+SUCCESS=0
+FAILURE=0
+AVG_SUCCESS_RATE=0
+while /usr/bin/true; do
+	echo "WARNING: clearing docker image cache in 10s...(CNTL-C to cancel)"
+	for i in {10..1}; do
+		echo -n $i...
+		sleep 1
+	done
+	echo
+	docker image rm $image
+	docker image prune --all --force
+	date_stamp=`date +%Y%M%d-%H%M`
+	(( ATTEMPT = ATTEMPT + 1 ))
+	echo "Starting attempt #${ATTEMPT} [$date_stamp]..."
+	docker pull $image
+	RESULT=$?
+	if [[ $RESULT == 0 ]]; then
+		(( SUCCESS = SUCCESS + 1 ))
+	else
+		(( FAILURE = FAIULURE + 1 ))
+	fi
+	(( AVG_SUCCESS_RATE = 100 * SUCCESS / ATTEMPT ))
+	echo "Result[${date_stamp}]: $RESULT (${ATTEMPT} attempts, ${AVG_SUCCESS_RATE}% success rate)" | tee -a $log
+done
